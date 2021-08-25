fb1326ac1c7735742165789e332c05e2ed37fe7b
Nakatsu <34512610+rodrigonakatsu@users.noreply.github.com>
2021-08-25 15:44:59 -0300

    Exercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'cursos';
+ 
+  nome;
+
+  alterarNome(evento){
+    console.log(evento.target.value)
+    this.nome = evento.target.value
+  }
+
+  adicionar(nome){
+    console.log(nome)
+    console.log("Adicionando...")
+  }
+
+ 
 }
